ba1f8ce2bf88699fc622ee00243d243882d26d30
charlieweinberger <charlieweinberger05@gmail.com>
2025-03-02 01:49:34 -0800

    Added title

--- src/pages/newtab/Newtab.tsx
+++ src/pages/newtab/Newtab.tsx
@@ -1,11 +1,14 @@
 import CalendarContainer from "@src/components/CalendarContainer";
 
 export default function Newtab() {
- const username: string = "charliebrown364";
- const graph: graphType = "yearly";
- return (
-   <div className="h-screen bg-primary-background flex items-center justify-center">
-     <CalendarContainer username={username} graph={graph} />
-   </div>
- );
+  const username: string = "charliebrown364";
+  const graph: graphType = "yearly";
+  return (
+    <div className="h-screen bg-primary-background flex flex-col gap-16 items-center">
+      <div className="mt-48 flex flex-col items-center gap-4 text-6xl text-primary-text font-bold">
+        Leetcode Calendar for <span className="text-leetcode-orange">{username}</span>
+      </div>
+      <CalendarContainer username={username} graph={graph} />
+    </div>
+  );
 }
