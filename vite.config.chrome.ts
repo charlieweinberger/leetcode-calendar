1141211376c3c9a8cb3c907d7716de966e9ab887
charlieweinberger <charlieweinberger05@gmail.com>
2025-03-13 02:27:31 -0700

    removed advanced analytics

--- vite.config.chrome.ts
+++ vite.config.chrome.ts
@@ -2,7 +2,6 @@ import { resolve } from 'path';
 import { mergeConfig, defineConfig } from 'vite';
 import baseConfig, { baseManifest, baseBuildOptions } from './vite.config.base';
 import { crx } from '@crxjs/vite-plugin';
-import { VitePluginRadar } from 'vite-plugin-radar';
 
 const outDir = resolve(__dirname, 'dist_chrome');
 
@@ -14,11 +13,6 @@ export default mergeConfig(
         manifest: baseManifest,
         browser: 'chrome',
       }),
-      VitePluginRadar({
-        analytics: {
-          id: "G-DMLCPS92H2",
-        },
-      })
     ],
     build: {
       ...baseBuildOptions,
